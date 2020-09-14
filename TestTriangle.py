86dfed3f528eb64e3a48733db745c777f8a6ba37
Jason Meyerberg <jmeyerbe@stevens.edu>
2020-09-14 17:33:30 -0400

    Code and tests Updat

--- TestTriangle.py
+++ TestTriangle.py
@@ -16,22 +16,36 @@ from Triangle import classifyTriangle
 
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
+	
+	#def testRightTriangle1(self): 
+	#	self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
 
-    def testRightTriangle1(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
-
-    def testRightTriangle2(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+	#def testRightTriangle2(self): 
+	#	self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
         
-    def testRightTriangle3(self): 
-        self.assertEqual(classifyTriangle(6,8,10),'Right','6,8,10 is a right triangle')
+	#def testRightTriangle3(self): 
+	#	self.assertEqual(classifyTriangle(6,8,10),'Right','6,8,10 is a right triangle')
 		
-    def testRightTriangle3(self): 
-        self.assertEqual(classifyTriangle(5,4,3),'Right','5,4,3 is a right triangle')
+	#def testRightTriangle4(self): 
+	#	self.assertEqual(classifyTriangle(5,4,3),'Right','5,4,3 is a right triangle')
 
-	def testRightTriangle3(self): 
-        self.assertNotEqual(classifyTriangle(1,1,1),'Right','1,1,1 is NOT a right triangle')
-
+	#def testRightTriangle5(self):
+	#	self.assertNotEqual(classifyTriangle(1,1,1),'Right','1,1,1 is NOT a right triangle')
+		
+	def testNotATriangle1(self):
+		self.assertEqual(classifyTriangle(1,10,1),'NotATriangle','1,10,1 is Not a triangle')
+	def testNotATriangle2(self):
+		self.assertEqual(classifyTriangle(1,5,2),'NotATriangle','1,5,2 is Not a triangle')
+	def testNotATriangle3(self):
+		self.assertEqual(classifyTriangle(6,5,1),'NotATriangle','6,5,1 is Not a triangle')
+	def testNotATriangle4(self):
+		self.assertEqual(classifyTriangle(30,10,10),'NotATriangle','30,10,10 is Not a triangle')
+	def testNotATriangle5(self):
+		self.assertNotEqual(classifyTriangle(4,3,2),'NotATriangle','4,3,2 is a triangle')
+		
+		
+		
+		
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
