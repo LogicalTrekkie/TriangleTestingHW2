e89516f9af5a5551e08113d1030e58459c2a8427
Jason Meyerberg <jmeyerbe@stevens.edu>
2020-09-14 17:46:47 -0400

    Test Update

--- TestTriangle.py
+++ TestTriangle.py
@@ -32,17 +32,26 @@ class TestTriangles(unittest.TestCase):
 	#def testRightTriangle5(self):
 	#	self.assertNotEqual(classifyTriangle(1,1,1),'Right','1,1,1 is NOT a right triangle')
 		
-	def testNotATriangle1(self):
-		self.assertEqual(classifyTriangle(1,10,1),'NotATriangle','1,10,1 is Not a triangle')
-	def testNotATriangle2(self):
-		self.assertEqual(classifyTriangle(1,5,2),'NotATriangle','1,5,2 is Not a triangle')
-	def testNotATriangle3(self):
-		self.assertEqual(classifyTriangle(6,5,1),'NotATriangle','6,5,1 is Not a triangle')
-	def testNotATriangle4(self):
-		self.assertEqual(classifyTriangle(30,10,10),'NotATriangle','30,10,10 is Not a triangle')
-	def testNotATriangle5(self):
-		self.assertNotEqual(classifyTriangle(4,3,2),'NotATriangle','4,3,2 is a triangle')
-		
+	#def testNotATriangle1(self):
+	#	self.assertEqual(classifyTriangle(1,10,1),'NotATriangle','1,10,1 is Not a triangle')
+	#def testNotATriangle2(self):
+	#	self.assertEqual(classifyTriangle(1,5,2),'NotATriangle','1,5,2 is Not a triangle')
+	#def testNotATriangle3(self):
+	#	self.assertEqual(classifyTriangle(6,5,1),'NotATriangle','6,5,1 is Not a triangle')
+	#def testNotATriangle4(self):
+	#	self.assertEqual(classifyTriangle(30,10,10),'NotATriangle','30,10,10 is Not a triangle')
+	#def testNotATriangle5(self):
+	#	self.assertNotEqual(classifyTriangle(4,3,2),'NotATriangle','4,3,2 is a triangle')
+	def testEquilateral1(self):
+		self.assertEqual(classifyTriangle(2,2,2),'Equilateral','2,2,2 is a Equilateral triangle')
+	def testEquilateral2(self):
+		self.assertNotEqual(classifyTriangle(3,3,2),'Equilateral','3,3,2 is NOT Equilateral triangle')
+	def testEquilateral3(self):
+		self.assertNotEqual(classifyTriangle(3,4,5),'Equilateral','3,4,5 is NOT Equilateral triangle')
+	def testEquilateral4(self):	
+		self.assertNotEqual(classifyTriangle(20,10,20),'Equilateral','20,10,20 is NOT Equilateral triangle')
+	def testEquilateral5(self):
+		self.assertNotEqual(classifyTriangle(1,2,2),'Equilateral','1,2,2 is Not Equilateral triangle')
 		
 		
 		
